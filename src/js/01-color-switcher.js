020b387f602e4ab23b5db39c0478692112f7c9a1
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-03-16 16:57:06 +0200

    add 01 hw

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const BtnStartEl = document.querySelector('button[data-start]');
+const BtnStopEl = document.querySelector('button[data-stop]');
+const bodyEl = document.body;
+let timerId = null;
 
+BtnStartEl.addEventListener('click', onBtnStart);
+BtnStopEl.addEventListener('click', onBtnStop);
+
+function onBtnStart () {
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+        BtnStartEl.disabled = true;
+    }, 1000);
+}
+
+function onBtnStop() {
+    clearInterval(timerId);
+    BtnStartEl.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
