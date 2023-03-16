a11c39b6bc90c7b7ce2ee4da4d79fff4a7625b11
YuriyBerketa <115158490+YuriyBerketa@users.noreply.github.com>
2023-03-16 14:57:32 +0000

    Deploying to gh-pages from @ YuriyBerketa/goit-js-hw-09@020b387f602e4ab23b5db39c0478692112f7c9a1 🚀

---
+++ 01-color-switcher.e66237ef.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body;let o=null;t.addEventListener("click",(function(){o=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`,t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.e66237ef.js.map
